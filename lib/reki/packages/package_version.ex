cc390659bb4d08821f8f999c345ac092220bbcca
ahmki <aleksi.heinimaki1@gmail.com>
2026-04-21 22:43:17 +0300

    package versions schema+migrations

---
+++ lib/reki/packages/package_version.ex
@@ -0,0 +1,40 @@
+defmodule Reki.Packages.PackageVersion do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+  schema "package_versions" do
+    field :package_id
+    field :version, :string, null: false
+    field :manifest, :map, null: false
+    field :tarball_url, :string
+    field :tarball_size, :integer
+    field :validation_results, :map, default: %{}
+
+    field :validation_status, Ecto.Enum,
+      values: [:pending, :approved, :blocked],
+      default: :pending
+
+    belongs_to :package, Reki.Packages.Package
+
+    timestamps(type: :utc_datetime)
+  end
+
+  @doc false
+  def changeset(version, attrs) do
+    version
+    |> cast(attrs, [
+      :package_id,
+      :version,
+      :manifest,
+      :tarball_url,
+      :tarball_size,
+      :validation_status,
+      :validation_results
+    ])
+    |> validate_required([:package_id, :version, :manifest])
+    |> assoc_constraint(:package)
+    |> unique_constraint([:package_id, :version])
+  end
+end
---
+++ priv/repo/migrations/20260421184340_create_package_versions.exs
@@ -0,0 +1,22 @@
+defmodule Reki.Repo.Migrations.CreatePackageVersions do
+  use Ecto.Migration
+
+  def change do
+    create table(:package_versions, primary_key: false) do
+      add :id, :binary_id, primary_key: true
+      add :package_id, references(:packages, type: :binary_id, on_delete: :delete_all), null: false
+      add :version, :string, null: false
+      add :manifest, :map, null: false
+      add :tarball_url, :string
+      add :tarball_size, :integer
+      add :validation_status, :string, default: "pending", null: false
+      add :validation_results, :map, default: %{}
+
+      timestamps()
+    end
+
+    create unique_index(:package_versions, [:package_id, :version])
+    create index(:package_versions, [:package_id])
+    create index(:package_versions, [:validation_status])
+  end
+end
